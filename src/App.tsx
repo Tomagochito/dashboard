628fed40268320148d93863677634400578be30e
Bolaños Fajardo Tomás Steven <tbolanos@espol.edu.ec>
2024-06-18 21:15:49 -0500

    4
    4

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+
 import './App.css'
-
+import Calculator from './components/Calculator';
+import Plan from './components/Plan';
+import Result from './components/Result';
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-	<Grid container spacing={5}>
-		<Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
-		<Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
-		<Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
-		<Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
-		<Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
-		<Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
-	  </Grid>
-  )
+	return (
+		<Grid container spacing={5}>
+			<Grid xs={12} sm={12} md={12} lg={12}>
+				<Calculator/>
+			</Grid>
+			<Grid xs={12} sm={6} md={6} lg={6}>
+				<Plan />
+			</Grid>
+			<Grid xs={12} sm={6} md={6} lg={6}>
+				<Result/>
+			</Grid>
+		</Grid>
+    )
 }
 
 export default App
