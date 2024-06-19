5d0de8cb032e90c1c80a367b9995790ad4300795
Bolaños Fajardo Tomás Steven <tbolanos@espol.edu.ec>
2024-06-18 21:49:32 -0500

    cambios
    a

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,30 @@ import Calculator from './components/Calculator';
 import Plan from './components/Plan';
 import Result from './components/Result';
 function App() {
+	
+		return (
+			<Grid container spacing={5}>
+				<Grid xs={12} sm={12} md={12} lg={12}>
+					<Calculator/>
+				</Grid>
+				<Grid xs={12} sm={6} md={6} lg={6}>
+					<Plan />
+				</Grid>
+				<Grid xs={12} sm={6} md={6} lg={6}>
+					<Result/>
+				</Grid>
+			</Grid>
+		)
 
-	return (
-		<Grid container spacing={5}>
-			<Grid xs={12} sm={12} md={12} lg={12}>
-				<Calculator/>
+		/*return (
+			<Grid container spacing={5}>
+				<Grid xs={3} lg={12}> 1 </Grid>
+				<Grid xs={3} lg={6}> 2 </Grid>
+				<Grid xs={3} lg={6}> 3 </Grid>
 			</Grid>
-			<Grid xs={12} sm={6} md={6} lg={6}>
-				<Plan />
-			</Grid>
-			<Grid xs={12} sm={6} md={6} lg={6}>
-				<Result/>
-			</Grid>
-		</Grid>
-    )
+		)*/
 }
+
+
 
 export default App
