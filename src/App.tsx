0fef68fe432b0eb76439f5efb7664fc5681ff47a
Tomagochito <tbolanos@espol.edu.ec>
2024-07-12 21:40:07 -0500

    para que se pueda hacer npm build y deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+{/*}
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import BasicTable from './components/BasicTable';
 
@@ -12,49 +13,40 @@ function App() {
 
 	let [rowsTable, setRowsTable] = useState([])
 	let [indicators, setIndicators] = useState([])
- {/* Hook: useEffect */}
-	useEffect(()=>{
 
-		(async ()=>{
+	useEffect(() => {
 
-			{/* Request */}
+		(async () => {
+
 
 			let API_KEY = "00dc3df108d7a72b939238e8683195e4"
 			let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
 			let savedTextXML = await response.text();
 
-			{/* 2. Del LocalStorage, obtiene el valor de las claves openWeatherMap y expiringTime */}
 
 			let savedTextXML2 = localStorage.getItem("openWeatherMap")
 			let expiringTime = localStorage.getItem("expiringTime")
 
-			{/* 3. Obtenga la estampa de tiempo actual */}
 
 			let nowTime = (new Date()).getTime();
 
-			{/* 4. Realiza la petición asicrónica cuando: 
-				(1) La estampa de tiempo de expiración (expiringTime) es nula, o  
-				(2) La estampa de tiempo actual es mayor al tiempo de expiración */}
 
-			if(expiringTime === null || nowTime > parseInt(expiringTime)) {
+			if (expiringTime === null || nowTime > parseInt(expiringTime)) {
 
-				{/* 5. Request */}
 
 				let API_KEY = "00dc3df108d7a72b939238e8683195e4"
 				let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
 				savedTextXML2 = await response.text();
 
 
-				{/* 6. Diferencia de tiempo */}
 
 				let hours = 1
 				let delay = hours * 3600000
 
 
-				{/* 7. En el LocalStorage, almacena texto en la clave openWeatherMap y la estampa de tiempo de expiración */}
 
 				localStorage.setItem("openWeatherMap", savedTextXML2)
-				localStorage.setItem("expiringTime", (nowTime + delay ).toString() )
+				localStorage.setItem("expiringTime", (nowTime + delay).toString())
 			}
 
 
@@ -62,73 +54,50 @@ function App() {
 			const parser = new DOMParser();
 			const xml = parser.parseFromString(savedTextXML, "application/xml");
 
-			{/* Arreglo para agregar los resultados */}
 
 			let dataToIndicators = new Array()
-
-			{/* 
-				Análisis, extracción y almacenamiento del contenido del XML 
-				en el arreglo de resultados
-			*/}
 
 			let location = xml.getElementsByTagName("location")[1]
 
 			let geobaseid = location.getAttribute("geobaseid")
-			dataToIndicators.push(["Location","geobaseid", geobaseid])
+			dataToIndicators.push(["Location", "geobaseid", geobaseid])
 
 			let latitude = location.getAttribute("latitude")
-			dataToIndicators.push(["Location","Latitude", latitude])
+			dataToIndicators.push(["Location", "Latitude", latitude])
 
 			let longitude = location.getAttribute("longitude")
-			dataToIndicators.push(["Location","Longitude", longitude])
-
-
-			{/* Renderice el arreglo de resultados en un arreglo de elementos Indicator */}
+			dataToIndicators.push(["Location", "Longitude", longitude])
 
 			let indicatorsElements = Array.from(dataToIndicators).map(
-			(element) => <Indicator title={element[0]} subtitle={element[1]} value={element[2]} />
+				(element) => <Indicator title={element[0]} subtitle={element[1]} value={element[2]} />
 			)
-			
-			{/* Modificación de la variable de estado mediante la función de actualización */}
 
 			setIndicators(indicatorsElements)
 
-			{/* 
-			2. Procese los resultados de acuerdo con el diseño anterior.
-			Revise la estructura del documento XML para extraer los datos necesarios. 
-			*/}
+			let arrayObjects = Array.from(xml.getElementsByTagName("time")).map((timeElement) => {
+				let rangeHours = timeElement.getAttribute("from").split("T")[1] + " - " + timeElement.getAttribute("to").split("T")[1]
+				let windDirection = timeElement.getElementsByTagName("windDirection")[0].getAttribute("deg") + " " + timeElement.getElementsByTagName("windDirection")[0].getAttribute("code")
+				return { "rangeHours": rangeHours, "windDirection": windDirection }
 
-			let arrayObjects = Array.from( xml.getElementsByTagName("time") ).map( (timeElement) =>  {
-						
-			let rangeHours = timeElement.getAttribute("from").split("T")[1] + " - " + timeElement.getAttribute("to").split("T")[1]
-
-			let windDirection = timeElement.getElementsByTagName("windDirection")[0].getAttribute("deg") + " "+  timeElement.getElementsByTagName("windDirection")[0].getAttribute("code") 
-					
-			return { "rangeHours": rangeHours,"windDirection": windDirection }
-				
 			})
 
-			arrayObjects = arrayObjects.slice(0,8)
+			arrayObjects = arrayObjects.slice(0, 8)
 			setRowsTable(arrayObjects)
 
 		})()
 
-	},[])
-
+	}, [])
 
 	return (
 		<Grid container paddingX={15} spacing={2} alignItems="center">
 			<Grid xs={6} md={4} lg={2}>
 				{indicators[0]}
-				{/* <Indicator title='Lunes' subtitle='Probabilidad' value={0.13} />*/}
 			</Grid>
 			<Grid xs={6} md={4} lg={2}>
 				{indicators[1]}
-				{/*<Indicator title='Martes' subtitle='Probabilidad' value={0.13} />*/}
 			</Grid>
 			<Grid xs={6} md={4} lg={2}>
 				{indicators[2]}
-				{/*<Indicator title='Miércoles' subtitle='Probabilidad' value={0.13} />*/}
 			</Grid>
 			<Grid xs={6} md={4} lg={2}>
 				<Indicator title='Jueves' subtitle='Probabilidad' value={0.13} />
@@ -139,35 +108,147 @@ function App() {
 			<Grid xs={6} md={4} lg={2}>
 				<Indicator title='Sábado' subtitle='Probabilidad' value={0.13} />
 			</Grid>
-
 			<Grid xs={12} lg={2}>
-
 				<Grid xs={6} lg={6} sx={({ paddingBottom: "5%" })}>
 					<Indicator title='Domingo' subtitle='Probabilidad' value={0.13} />
 				</Grid>
-
 				<Grid xs={6} lg={6} sx={({ paddingBottom: "5%" })}>
 					<Summary></Summary>
 				</Grid>
-
 			</Grid>
 			<Grid xs={12} md={6} lg={10} >
-			<BasicTable rows={rowsTable}></BasicTable>
+				<BasicTable rows={rowsTable}></BasicTable>
 			</Grid>
-
 			<Grid xs={12} lg={2}>
 				<ControlPanel />
 			</Grid>
-
 			<Grid xs={12} lg={10}>
 				<WeatherChart></WeatherChart>
 			</Grid>
-
 		</Grid>
 	)
+}
+export default App
+*/}
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import BasicTable from './components/BasicTable';
+import './App.css'
+import Indicator from './components/Indicator';
+import Summary from './components/Summary';
+import WeatherChart from './components/WeatherChart';
+import ControlPanel from './components/ControlPanel';
+import { useState, useEffect } from 'react';
 
+interface Row {
+  rangeHours: string;
+  windDirection: string;
 }
 
+function App() {
+  let [rowsTable, setRowsTable] = useState<Row[]>([]);
+  let [indicators, setIndicators] = useState<JSX.Element[]>([]);
 
+  useEffect(() => {
+    (async () => {
+      let API_KEY = "00dc3df108d7a72b939238e8683195e4";
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`);
+      let savedTextXML = await response.text();
 
-export default App
+      let savedTextXML2 = localStorage.getItem("openWeatherMap");
+      let expiringTime = localStorage.getItem("expiringTime");
+
+      let nowTime = (new Date()).getTime();
+
+      if (expiringTime === null || nowTime > parseInt(expiringTime)) {
+        response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`);
+        savedTextXML2 = await response.text();
+
+        let hours = 1;
+        let delay = hours * 3600000;
+
+        localStorage.setItem("openWeatherMap", savedTextXML2);
+        localStorage.setItem("expiringTime", (nowTime + delay).toString());
+      }
+
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(savedTextXML, "application/xml");
+
+      let dataToIndicators = new Array();
+      let location = xml.getElementsByTagName("location")[1];
+
+      let geobaseid = location.getAttribute("geobaseid");
+      dataToIndicators.push(["Location", "geobaseid", geobaseid]);
+
+      let latitude = location.getAttribute("latitude");
+      dataToIndicators.push(["Location", "Latitude", latitude]);
+
+      let longitude = location.getAttribute("longitude");
+      dataToIndicators.push(["Location", "Longitude", longitude]);
+
+      let indicatorsElements = Array.from(dataToIndicators).map(
+        (element) => <Indicator title={element[0]} subtitle={element[1]} value={element[2]} />
+      );
+
+      setIndicators(indicatorsElements);
+
+      let arrayObjects = Array.from(xml.getElementsByTagName("time")).map((timeElement) => {
+        let fromAttr = timeElement.getAttribute("from");
+        let toAttr = timeElement.getAttribute("to");
+        if (fromAttr && toAttr) {
+          let rangeHours = fromAttr.split("T")[1] + " - " + toAttr.split("T")[1];
+          let windDirection = timeElement.getElementsByTagName("windDirection")[0].getAttribute("deg") + " " + timeElement.getElementsByTagName("windDirection")[0].getAttribute("code");
+          return { rangeHours, windDirection };
+        } else {
+          return null;
+        }
+      }).filter(item => item !== null) as Row[];
+
+      arrayObjects = arrayObjects.slice(0, 8);
+      setRowsTable(arrayObjects);
+    })();
+  }, []);
+
+  return (
+    <Grid container paddingX={15} spacing={2} alignItems="center">
+      <Grid xs={6} md={4} lg={2}>
+        {indicators[0]}
+      </Grid>
+      <Grid xs={6} md={4} lg={2}>
+        {indicators[1]}
+      </Grid>
+      <Grid xs={6} md={4} lg={2}>
+        {indicators[2]}
+      </Grid>
+      <Grid xs={6} md={4} lg={2}>
+        <Indicator title='Jueves' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={6} md={4} lg={2}>
+        <Indicator title='Viernes' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={6} md={4} lg={2}>
+        <Indicator title='Sábado' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+
+      <Grid xs={12} lg={2}>
+        <Grid xs={6} lg={6} sx={{ paddingBottom: "5%" }}>
+          <Indicator title='Domingo' subtitle='Probabilidad' value={0.13} />
+        </Grid>
+        <Grid xs={6} lg={6} sx={{ paddingBottom: "5%" }}>
+          <Summary></Summary>
+        </Grid>
+      </Grid>
+      <Grid xs={12} md={6} lg={10}>
+        <BasicTable rows={rowsTable}></BasicTable>
+      </Grid>
+
+      <Grid xs={12} lg={2}>
+        <ControlPanel />
+      </Grid>
+      <Grid xs={12} lg={10}>
+        <WeatherChart></WeatherChart>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
--- src/components/BasicTable.tsx
+++ src/components/BasicTable.tsx
@@ -1,8 +1,8 @@
+{/*
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import { useState, useEffect } from "react"
 
-{/* 3. Declare la interfaz del prop de entrada */ }
 
 interface Config {
   rows: Array<object>;
@@ -10,17 +10,8 @@ interface Config {
 
 export default function BasicTable(data: Config) {
 
-
-  {/* 
-         4. Declare la variable de estado (rows) y la función de actualización (setRows).
-         Use el mismo identificador de la variable con valores fijos (rows)
-     */}
-
   let [rows, setRows] = useState([])
-  {/* 
-         5. Agregue el hook useEffect, controlado por el prop del componente (data), y
-         Dentro del hook, invoque al métdo de actualización con el valor del prop (data.rows).
-     */}
+
 
   useEffect(() => {
 
@@ -33,21 +24,17 @@ export default function BasicTable(data: Config) {
   }, [data])
 
 
-
-  {/* JSX */ }
   return (
 
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            {/* Modifique la cabecera de la tabla con los títulos adecuados */}
             <TableCell>Rango de horas</TableCell>
             <TableCell align="right">Dirección del viento</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {/* Modifique las filas de la tabla con las claves rangeHours y windDirection del objeto  */}
           {rows.map((row) => (
             <TableRow
               key={row.rangeHours}
@@ -64,5 +51,54 @@ export default function BasicTable(data: Config) {
     </TableContainer>
 
 
+  );
+}
+
+*/}
+
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { useState, useEffect } from "react";
+
+interface Row {
+  rangeHours: string;
+  windDirection: string;
+}
+
+interface Config {
+  rows: Row[];
+}
+
+export default function BasicTable({ rows }: Config) {
+  let [rowsState, setRowsState] = useState<Row[]>([]);
+
+  useEffect(() => {
+    setRowsState(rows);
+  }, [rows]);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Rango de horas</TableCell>
+            <TableCell align="right">Dirección del viento</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rowsState.map((row) => (
+            <TableRow
+              key={row.rangeHours}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.rangeHours}
+              </TableCell>
+              <TableCell align="right">{row.windDirection}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
